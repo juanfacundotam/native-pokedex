37cb0963cc04432785c1e82629c2f4537decc2ea
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-14 17:27:39 -0300

    Create Favorite.js

---
+++ src/screens/Favorite.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Favorite() {
+  return (
+    <SafeAreaView>
+      <Text>Favorite</Text>
+    </SafeAreaView>
+  )
+}
