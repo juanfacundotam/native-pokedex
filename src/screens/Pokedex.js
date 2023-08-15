77bfa4323f63c357c197f4c92e77bed7b9884d48
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-15 16:57:02 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,24 @@
 import { View, Text } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { getPokemonApi } from '../api/pokemon';
 
 export default function Pokedex() {
+  useEffect(()=>{
+    (async () => {
+      await loadPokemons();
+    })();
+    console.log("hola mundo")
+  },[])
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
