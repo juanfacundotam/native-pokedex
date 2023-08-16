0e20ba791b35b7d745a19a0e7aa451dec5e35242
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 15:53:04 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,20 +1,21 @@
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native'
-import { SafeAreaView } from "react-native-safe-area-context";
-import React, {useState, useEffect} from 'react'
-import { getPokemonApi, getPokemonDetailsByUrlApi } from '../api/pokemon';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text } from "react-native";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
-  const [pokemons, setPokemons] = useState([])
-  console.log(pokemons)
-  useEffect(()=>{
+  const [pokemons, setPokemons] = useState([]);
+  console.log("pokemons--->", pokemons);
+
+  useEffect(() => {
     (async () => {
       await loadPokemons();
     })();
-  },[])
+  }, []);
 
-  const loadPokemons = async (url) => {
+  const loadPokemons = async () => {
     try {
-      const response = await getPokemonApi();
+      const response = await getPokemonsApi();
+
       const pokemonsArray = [];
       for await (const pokemon of response.results) {
         const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
@@ -26,55 +27,18 @@ export default function Pokedex() {
           order: pokemonDetails.order,
           imagen:
             pokemonDetails.sprites.other["official-artwork"].front_default,
-        })
-
+        });
       }
-      setPokemons([...pokemons, pokemonsArray])
-      console.log(pokemons)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-  return (
-      <ScrollView style={styles.scrollContainer}>
-        {pokemons.map((pokemon, index) => (
-          <View key={index} style={styles.pokemonContainer}>
-            <Image
-              source={{ uri: pokemon.imagen }}
-              style={styles.pokemonImage}
-            />
-            <View style={styles.pokemonText}>
-              <Text>{pokemon.name}</Text>
-              <Text>{pokemon.type}</Text>
-              <Text>{pokemon.order}</Text>
-            </View>
-          </View>
-        ))}
-      </ScrollView>
-  )
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  scrollContainer: {
-    // flexDirection: "row",
-    // flexWrap: "wrap",
-  },
-  pokemonContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  pokemonText: {
-    marginLeft: 2,
-  },
-  pokemonImage: {
-    width: 100,
-    aspectRatio: 1,
-  },
-});
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>Pokedex</Text>
+    </SafeAreaView>
+  );
+}
