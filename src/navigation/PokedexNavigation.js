7381c15760d38be0df3a4623da3b8069181fe14d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 15:52:58 -0300

    Update PokedexNavigation.js

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -1,20 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Pokedex from "../screens/Pokedex";
+import PokedexScreen from "../screens/Pokedex";
+import PokemonScreen from "../screens/Pokemon";
 
 const Stack = createStackNavigator();
 
 export default function PokedexNavigation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Pokedex"
-        component={Pokedex}
-        options={{
-          title: "Pokedex",
-          headerTitleAlign: "center",
-        }}
-      />
+      <Stack.Screen name="Pokedex" component={PokedexScreen} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} />
     </Stack.Navigator>
   );
 }
