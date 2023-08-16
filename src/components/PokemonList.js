c300343273caf7ecd777de17fd51e4eb0da3306c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 17:46:22 -0300

    Update PokemonList.js

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -9,7 +9,7 @@ export default function PokemonList(props) {
 
   return (
     <FlatList
-      styles={{ height: "100%", width: "100%" }}
+      // styles={{ height: "100%", width: "100%" }}
       data={pokemons}
       numColumns={2}
       showsVerticalScrollIndicator={false} // Para que no aparezca la barra de scroll
@@ -23,8 +23,8 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
-    fontSize: 16,
-    height: "100%",
-    width: "100%",
+    // fontSize: 16,
+    // height: "100%",
+    // width: "100%",
   },
 });
