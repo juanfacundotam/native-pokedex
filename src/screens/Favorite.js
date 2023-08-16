d39bfc3dfdf4ff4eae032be919fdcc0829110c2c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 15:53:02 -0300

    Update Favorite.js

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,11 +1,10 @@
-import { View, Text} from 'react-native'
-import { SafeAreaView } from "react-native-safe-area-context";
-import React from 'react'
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
 
 export default function Favorite() {
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
     </SafeAreaView>
-  )
+  );
 }
