b2a8e8860deca2786381ea8e11b2a583170e6522
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-17 18:34:29 -0300

    Update Pokemon.js

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
+import { getPokemonDetailsApi } from "../api/pokemon";
 
 export default function Pokemon(props) {
-  const { route, navigation } = props;
+  const {
+    route: { params },
+    navigation,
+  } = props;-
+  const [pokemon, setPokemon] = useState(null);
+  console.log(params.id);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        console.log(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params.id]);
+
   return (
     <View>
       <Text>Estamos en un POKEMON</Text>
