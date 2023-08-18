7fdf63eadb77a6d9e488d9d5536b7e7868ae0828
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-18 17:47:08 -0300

    Update Pokemon.js

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
+import Type from "../components/Pokemon/Type";
+import Stats from "../components/Pokemon/Stats";
 
 export default function Pokemon(props) {
   const {
@@ -31,6 +33,8 @@ export default function Pokemon(props) {
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
+      <Type types={pokemon.types}/>
+      <Stats stats={pokemon.stats}/>
     </ScrollView>
   );
 }
