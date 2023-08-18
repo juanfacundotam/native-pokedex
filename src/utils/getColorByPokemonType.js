fa7b552a9ca530a18550d86ee5c170d965796787
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-18 16:47:13 -0300

    Update getColorByPokemonType.js

--- src/utils/getColorByPokemonType.js
+++ src/utils/getColorByPokemonType.js
@@ -1,5 +1,5 @@
 import { POKEMON_TYPE_COLORS } from "./constants";
 
-const getColorByPokemonType = (type) => POKEMON_TYPE_COLORS[type.toLowerCase()]
+const getColorByPokemonType = (type) => POKEMON_TYPE_COLORS[type.toLowerCase()];
 
 export default getColorByPokemonType;
