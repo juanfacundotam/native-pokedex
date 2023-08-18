243595132f8487860b2c148cca975b4962f78ef4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-18 18:07:42 -0300

    Update Pokemon.js

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
 import { getPokemonDetailsApi } from "../api/pokemon";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
 
 export default function Pokemon(props) {
+  
   const {
     navigation,
     route: { params },
   } = props;
+
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => <Icon name="arrow-left" color="#fff" size={20} style={{marginLeft: 20}} onPress={navigation.goBack}/>,
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
@@ -33,8 +43,8 @@ export default function Pokemon(props) {
         image={pokemon.sprites.other["official-artwork"].front_default}
         type={pokemon.types[0].type.name}
       />
-      <Type types={pokemon.types}/>
-      <Stats stats={pokemon.stats}/>
+      <Type types={pokemon.types} />
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
