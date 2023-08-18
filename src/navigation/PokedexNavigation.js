429b7b3471ff56f84d425b82051ab5fb738ea359
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-18 16:47:05 -0300

    Update PokedexNavigation.js

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -8,8 +8,16 @@ const Stack = createStackNavigator();
 export default function PokedexNavigation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Pokedex" component={PokedexScreen} options={{title: "", headerTransparent: true}}/>
-      <Stack.Screen name="Pokemon" component={PokemonScreen} />
+      <Stack.Screen
+        name="Pokedex"
+        component={PokedexScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={PokemonScreen}
+        options={{ title: "", headerTransparent: true }}
+      />
     </Stack.Navigator>
   );
 }
