e37dcee58eb9cc260d746cc856ee598bcce414cf
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-25 11:42:27 -0300

    Update Account.js

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { View, Text } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
+import useAuth from "../hooks/useAuth";
+
 
 export default function Account() {
+  const {auth} = useAuth();
+ 
   return (
-    <SafeAreaView>
-      <Text>Account</Text>
-    </SafeAreaView>
+    <View>
+      {auth ? <UserData/> : <LoginForm/>}
+    </View>
   );
 }
