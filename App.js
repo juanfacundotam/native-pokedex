a362983315b5f100a8fe98317b883632028453df
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-25 11:42:41 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
+import { AuthProvider } from "./src/context/AuthContext";
 export default function App() {
   return (
     <NavigationContainer>
+      <AuthProvider>
       <Navigation />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
