4c1f6dbbb6f47b5859ed4ce95310e12d935250a3
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-25 11:42:24 -0300

    Create userDB.js

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+    username: "poke",
+    password: "poke"
+}
+
+export const userDetails = {
+    username: "poke",
+    fistName: "Pokedex",
+    lastName: "Pokemon",
+    email: "pokemon@pokemon.com"
+}
