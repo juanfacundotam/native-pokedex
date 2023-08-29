261845a686e3097f2c578f52ca69920349ff32b4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-29 12:37:00 -0300

    Revert "Update Header.js"
    This reverts commit 8b13b2f88c208b46dac2c853e6cc10fd5d7178df.

--- src/components/Pokemon/Header.js
+++ src/components/Pokemon/Header.js
@@ -29,7 +29,7 @@ export default function Header(props) {
 const styles = StyleSheet.create({
   bg: {
     width: "100%",
-    height: 400,
+    height: 420,
     position: "absolute",
     borderBottomEndRadius: 300,
     borderBottomLeftRadius: 300,
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    paddingTop: 40,
+    paddingTop: 45,
   },
   name: {
     color: "#fff",
