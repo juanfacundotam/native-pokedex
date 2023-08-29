9530c8e23b67b3e2a3d9720e20b3ec833fe1466c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-29 13:40:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/context/AuthContext";
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <AuthProvider>
       <Navigation />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
+
+
+
