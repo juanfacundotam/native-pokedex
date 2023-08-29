b0a5564c4baa6895b4460be12afcf0605911855e
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-29 12:37:25 -0300

    Revert "Revert "Update App.js""
    This reverts commit 8077a4c60d0342b2f92be7a463dcbcdf02e04527.

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
 import { AuthProvider } from "./src/context/AuthContext";
+
 export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
-      <Navigation />
+        <Navigation />
       </AuthProvider>
     </NavigationContainer>
   );
