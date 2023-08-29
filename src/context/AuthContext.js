48d9e2d8927fee604e884b876f5327da35967792
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-29 12:02:49 -0300

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,12 +10,13 @@ export function AuthProvider(props) {
   const { children } = props;
   const [auth, setAuth] = useState(undefined);
 
-    const login = (userData) => {
-        setAuth(userData)
-    }
-    const logout = () => {
-        setAuth(undefined)
-    }
+  const login = (userData) => {
+    setAuth(userData);
+  };
+
+  const logout = () => {
+    setAuth(undefined);
+  };
 
   const valueContext = {
     auth,
