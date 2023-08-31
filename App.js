3bf67cfca7dff7e0d587768c6dce63d193545730
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-31 13:40:37 -0300

    updated

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/context/AuthContext";
 import Navigation from "./src/navigation/Navigation";
+import 'expo-dev-client';
 
 export default function App() {
   return (
